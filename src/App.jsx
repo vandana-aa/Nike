efbc3537b18296120e95b041ce9d8565aa3cb6ee
dsa_questions <vandana3105s@gmail.com>
2023-12-11 05:07:02 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+//import Hero from './sections/Hero';
+//import Nav from './sections/Nav';
+//import PopularProducts from './sections/PopularProducts';
+import{
+  Hero,
+    PopularProducts,
+    Nav,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+} from './sections'
+
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="x1:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffer
+    </section>
+    <section className="bg-pale-blue padding">
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      Footer
+    </section>
+  </main>
+);
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from './Hero';
+import Nav from './Nav';
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import Footer from './footer';
+import Services from './Services';
+import SuperQuality from './SuperQuality';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+
+export{
+    Hero,
+    PopularProducts,
+    Nav,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
