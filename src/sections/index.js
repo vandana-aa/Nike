ff0d259bf2609ecd90f8f00a224cdc974bde6c5c
dsa_questions <vandana3105s@gmail.com>
2023-12-11 17:42:46 +0530

    update1

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,21 +1,19 @@
-import Hero from './Hero';
-import Nav from './Nav';
-import CustomerReviews from './CustomerReviews';
-import PopularProducts from './PopularProducts';
-import Footer from './footer';
-import Services from './Services';
-import SuperQuality from './SuperQuality';
-import SpecialOffer from './SpecialOffer';
-import Subscribe from './Subscribe';
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import Footer from "./footer";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
 
-export{
-    Hero,
-    PopularProducts,
-    Nav,
-    SuperQuality,
-    Services,
-    SpecialOffer,
-    CustomerReviews,
-    Subscribe,
-    Footer
-}
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+};
